c05ef26e2933e6081c50b3b3c8b1dccc9e758fc0
Krovannox <krovannox@proton.me>
2026-04-03 10:52:26 -0500

    Add function that place markers on the array

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 /* Simplified board using only an array with the 9 positions of the 3x3 board */
 const gameboard = Array(9).fill(null);
 console.log(gameboard);
+
+/* Function to place the markers in the gameboard array */
+function placeMarker(position, marker) {
+    /* Check if there's already a marker in that position */
+    if (gameboard[position] !== null) return;
+
+    gameboard[position] = marker;
+    console.log(gameboard);
+}
