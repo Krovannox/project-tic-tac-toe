c586c3b8fdfb07c23fd33f74c0dbf60dbd0f2c23
Krovannox <krovannox@proton.me>
2026-04-03 10:40:03 -0500

    Rework the gameboard declaration
    The initial idea was to use an array of arrays to represent the 3x3
    grid, however it would be simpler to just use a regular array with 9
    positions and track them with the equivalent number placing of the
    original grid, this way the check for the winning condition could be
    more simpler.

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-/* Create a 3x3 Array */
-function createGameboard() {
-    /* Create an empty object */
-    const gameboard = {};
-
-    /* Define the number of rows and columns for the board */
-    const rows = 3;
-    const columns = 3;
-
-    /* Create the arrays using a nested for loop */
-    for (let i = 0; i < rows; i++) {
-        gameboard[i] = [];
-
-        for (let j = 0; j < columns; j++) {
-            gameboard[i][j] = 0;
-        }
-    }
-
-    return gameboard;
-}
-
-console.table(createGameboard());
+/* Simplified board using only an array with the 9 positions of the 3x3 board */
+const gameboard = Array(9).fill(null);
+console.log(gameboard);
